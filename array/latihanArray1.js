ccc86329354fc05ac135c1513e1e50edf1a64ef8
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-03-27 14:41:17 +0700

    selesai latihan array

--- array/latihanArray1.js
+++ array/latihanArray1.js
@@ -1,6 +1,6 @@
 // Program sederhana data penumpang angkot
 
-let penumpang = ["Ilham", undefined, "Budi"];
+let penumpang = [];
 
 let tambahPenumpang = (namaPenumpang, penumpang) => {
   // Jika angkot kosong
@@ -27,6 +27,29 @@ let tambahPenumpang = (namaPenumpang, penumpang) => {
       //   Jika nama penumpang sudah ada di dalam angkot
       else if (penumpang[i] == namaPenumpang) {
         console.log(`${namaPenumpang} sudah ada di dalam angkot!`);
+        return penumpang;
+      }
+    }
+  }
+};
+
+let hapusPenumpang = (namaPenumpang, penumpang) => {
+  // Jika angkot kosong tampilkan pesan bahwa angkot kosong
+  if (penumpang.length == 0) {
+    console.log(`Angkot Masih Kosong!`);
+    return penumpang;
+  } // telusuri seluruh penumpang
+  else {
+    for (let i = 0; i < penumpang.length; i++) {
+      // Jika penumpang ditemukan
+      if (penumpang[i] == namaPenumpang) {
+        // hapus penumpang dengan mengisi dengan undefined
+        penumpang[i] = undefined;
+        // kembalikan array penupang
+        return penumpang;
+      } // Jika penumpang tidak ditemukan
+      else if (i == penumpang.length - 1) {
+        console.log(`${namaPenumpang} sudah keluar dari angkot!`);
         return penumpang;
       }
     }
